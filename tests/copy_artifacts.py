79594f252d29d21ead48bfa1e64f60622e81ed19
Matthieu Buffet <matthieu@buffet.re>
2023-10-30 01:11:25 +0100

    Replace test copy script

--- tests/copy_artifacts.py
+++ tests/copy_artifacts.py
@@ -5,17 +5,33 @@ import shutil
 import sys
 from pathlib import Path
 
-target_dir = Path(sys.argv[1])
+ARTEFACT_TYPES = ['test', 'bin', 'cdylib']
+EXCLUDE_EXTS = ['.pdb']
+
+target_rootdir = Path(sys.argv[1])
 for line in sys.stdin:
     try:
         obj = json.loads(line)
     except:
         continue
-    if type(obj) is not dict or obj.get('executable', None) is None:
+    if type(obj) is not dict:
         continue
-    if 'test' in obj.get('target', {}).get('kind', []):
-        shutil.copy(obj['executable'], target_dir)
-    elif 'bin' in obj.get('target', {}).get('kind', []):
-        shutil.copy(obj['executable'], target_dir / 'workers')
-    elif 'cdylib' in obj.get('target', {}).get('kind', []):
-        shutil.copy(obj['executable'], target_dir / 'workers')
+    kinds = obj.get('target', {}).get('kind', [])
+    for kind in ARTEFACT_TYPES:
+        if kind in kinds:
+            break
+    else:
+        continue
+    # We need to give a way for the CI runner to
+    # differentiate between actual tests and worker
+    # binaries: put them in a subdirectory
+    if 'bin' in kinds or 'cdylib' in kinds:
+        target_dir = target_rootdir / 'workers'
+    else:
+        target_dir = target_rootdir
+    for filename in obj.get('filenames', []):
+        for ext in EXCLUDE_EXTS:
+            if filename.lower().endswith(ext):
+                break
+        else:
+            shutil.copy(filename, target_dir)
